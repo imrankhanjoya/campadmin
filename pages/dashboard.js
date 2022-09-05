eddc43e65968337686421c5d671c659bfe70554e
Akramjoya <107855423+Akramjoya@users.noreply.github.com>
2022-09-05 17:14:55 +0530

    changes

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboardtable from '../Components/Dashboardtable'
+
+function Dashboard() {
+  return (
+    <div className='container'>
+        <Dashboardtable/>
+    </div>
+  )
+}
+
+export default Dashboard
